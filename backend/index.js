d94a25526ab2de0cbd38131e46fd3d38f655e296
Srujan <srujandivakar07@gmail.com>
2025-04-13 23:05:21 +0530

    corsoption&get_req

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,26 @@
 
 import express, { urlencoded } from "express"; // to import, add type in .json file
 import cookieParser from "cookie-parser";
+import cors from "cors"; 
 
 const PORT = 3000;
 const app = express();
+
+// API (basic get request)
+/*app.get("/home",(req,res)=>{
+    return res.status(200).json({
+        message:"I am coming from backend",
+        success:true,
+    })
+});*/
+
+// cors is used to communicate between differnt host (backend host & frontend host)
+const corsOption = {
+    origin :`http//localhost:5173`,
+    Credential:true
+}
+
+app.use(cors(corsOption));
 
 app.use(express.json());
 app.use(urlencoded({extended:true}));
